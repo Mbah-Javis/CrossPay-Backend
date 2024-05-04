fefda00eccf14adc8ae3229651aab1e68942f52a
Javis <jelvismbah18@gmail.com>
2024-05-04 19:18:16 +0100

    Update verify_source.js

--- middleware/verify_source.js
+++ middleware/verify_source.js
@@ -1,11 +1,11 @@
-const createLogger = require('../utils/crosspay_logger');
+const crossPayLogger = require('../utils/crosspay_logger');
 const crossPayResponse = require('../utils/crosspay_response');
 
 module.exports = (req, res, next) => {
   const secretHash = process.env.CROSSPAY_SECRET_HASH
   const signature = req.headers['verify-crosspay-hash']
   if (!signature || signature !== secretHash) {
-    createLogger.warn('unauthorized', [signature]);
+    crossPayLogger.warn('unauthorized', [signature]);
     return crossPayResponse.sendErrorResponse(res, 'Unauthorized', 401).end();
   } else {
     return next()
