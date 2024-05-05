36c39defb556af6edf169490f3f92aeed43c3842
Javis <jelvismbah18@gmail.com>
2024-05-05 15:30:38 +0100

    save user data service

--- services/crosspay_user_service.js
+++ services/crosspay_user_service.js
@@ -7,6 +7,10 @@ class CrossPayUserService {
     const ref = await db.collection(this.users).doc(uid).get()
     return ref.exists ? ref.data() : null
   }
+
+  static async saveUserData (uid, userData) {
+    return await db.collection(this.users).doc(uid).set(userData);
+  }
 }
 
 module.exports = CrossPayUserService
