845a6c5260e767799d051c25b8ef884dc74df437
Javis <jelvismbah18@gmail.com>
2024-05-13 17:25:33 +0100

    refactored and removed unused code

--- controllers/money-transfer/flutterwave/complete_transaction.js
+++ controllers/money-transfer/flutterwave/complete_transaction.js
@@ -32,13 +32,15 @@ const completeTransaction = async (req, res) => {
             await CrossPayTransactionService.updateUserDeliveredStatus(uuid, user_id, status)
 
             // Send transaction delivered notification
+
+            res.end()
         }
         // Update mobile momey charge transaction status
         const status = TransactionStatus.COMPLETED
         await CrossPayTransactionService.updateTransactionStatus(uuid, status)
         await CrossPayTransactionService.updateUserTransactionStatus(uuid, user_id, status)
 
-        // Fund virtual subaccount wallet account
+        // Fund virtual subaccount wallet
 
         // Send Money to receiver phone number
         // Check if transaction has already been initiated/completed to prevent 
@@ -70,27 +72,7 @@ const completeTransaction = async (req, res) => {
           }
 
           // Send money from wallet balance
-          /* const user = await CrossPayUserService.getUser(user_id)
-          const details = {
-              account_bank: operator,
-              account_number: `${contry_code}${receiver_number}`,
-              amount: receive_amount,
-              currency: receiver_currency,
-              beneficiary_name: receiver_name,
-              debit_currency: currency,
-              debit_subaccount: user.sub_account.account_reference,
-              reference: tx_ref,
-              meta: response.data.meta
-          };
-          const transfer = await FlutterwaveService.initiateTransfer(details)
-          crossPayLogger.info('Flutterwave transfer', [transfer])
-          if(transfer.data.status === 'NEW') {
-              const status = TransactionStatus.INPROGRESS
-              await CrossPayTransactionService.updateDeliveredStatus(uuid, status)
-              await CrossPayTransactionService.updateUserDeliveredStatus(uuid, user_id, status)
-  
-              // Send transaction in progress notification
-          } */
+
         }
         res.end()
     } else {
@@ -100,7 +82,9 @@ const completeTransaction = async (req, res) => {
         await CrossPayTransactionService.updateUserTransactionStatus(uuid, user_id, status)
         await CrossPayTransactionService.updateDeliveredStatus(uuid, status)
         await CrossPayTransactionService.updateUserDeliveredStatus(uuid, user_id, status)
+
         // Send failed notification to sender
+
         res.end()
     }
 
--- controllers/user/auth/add_new_user.js
+++ controllers/user/auth/add_new_user.js
@@ -33,12 +33,10 @@ const addNewUser = async (req, res) => {
         account_reference: null,
         default_currency: currency,
         wallet_balance: {
-          ngn: null,
           [currency]: null
         },
         wallet_details: {
           [currency]: null,
-          ngn: null,
         }
       },
       transactions: {
@@ -49,31 +47,6 @@ const addNewUser = async (req, res) => {
 
     /* Create user account without any wallet and perform transfers from main flutterwave account balance */
     await CrossPayUserService.saveUserData(uid, user)
-    
-  /*   // Create virtual  subaccount for funding and make transfers without using main account balance
-    const accountName = `${first_name} ${last_name}`
-    const mobileNumber = `${contry_code}${phone_number}`
-    const subAccount = await FlutterwaveService.createSubaccount(accountName, mobileNumber, email, country)
-    crossPayLogger.debug('Create Flutterwave subaccount', [subAccount])
-    if (subAccount.data.status === 'success') {
-      const accountReference = subAccount.data.data.account_reference
-      user.sub_account.id = subAccount.data.data.id
-      user.sub_account.account_reference = accountReference
-      await CrossPayUserService.saveUserData(uid, user)
-      
-      // Create virtual wallets for funding. NGN is created by default
-      const subAc = await FlutterwaveService.createWallet(accountReference, default_currency)
-      user.sub_account.wallet_details[currency] = subAc.data.status === 'success' ? subAc.data.data : null;
-      
-      // Get virtual wallets balance
-      const dfWallet = await FlutterwaveService.getWalletBalance(accountReference, default_currency)
-      user.sub_account.wallet_balance[currency] = dfWallet.data.status === 'success' ? dfWallet.data.data : null
-      const ngnWallet = await FlutterwaveService.getWalletBalance(accountReference, 'NGN')
-      user.sub_account.wallet_balance.ngn = ngnWallet.data.status === 'success' ? ngnWallet.data.data : null
-
-      await CrossPayUserService.updateWalletBalance(uid, currency, user.sub_account.wallet_balance[currency])
-      await CrossPayUserService.updateWalletBalance(uid, 'ngn', user.sub_account.ngn)
-    }  */
     
     const userData = await CrossPayUserService.getUser(uid)
     // Send welcome notification
